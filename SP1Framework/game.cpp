814d6c59f1492d54e7e1f10f994080a26c370198
Jenny liang <jenny_liang_leran@hotmail.com>
2014-08-25 16:49:37 +0800

    added borders and hearts

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -29,10 +29,10 @@ void init()
     // Set precision for floating point output
     std::cout << std::fixed << std::setprecision(3);
 
-	SetConsoleTitle(L"SP1 Framework");
+	SetConsoleTitle(L"Fruit Bomb");
 
 	// Sets the console size, this is the biggest so far.
-	setConsoleSize(79, 28);
+	setConsoleSize(79, 58);
 
     // Get console width and height
     CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */     
@@ -43,8 +43,8 @@ void init()
     consoleSize.Y = csbi.srWindow.Bottom + 1;
 
     // set the character to be in the center of the screen.
-    charLocation.X = consoleSize.X / 2;
-    charLocation.Y = consoleSize.Y / 2;// Mr Sim's
+    charLocation.X = (consoleSize.X - 10) / 2;
+    charLocation.Y = consoleSize.Y - 2;// Mr Sim's
 
 	ImplementObjects();
 
@@ -87,7 +87,7 @@ void update(double dt)
     if (keyPressed[K_UP] && charLocation.Y > 0 && jump == ENABLED)
     {
 		 Beep(1440, 30);
-		 charLocation.Y--; 
+		 charLocation.Y-= 6; 
 		 jump = jumptime;
     }
 	//Shannon : Player will stay in the air for number of frames of jumptime
@@ -98,7 +98,7 @@ void update(double dt)
 	//Shannon : Player will fall just before jumping is enabled again
 	if (jump == 1)
 	{
-		charLocation.Y++;
+		charLocation.Y+=6;
 	}
 
     if (keyPressed[K_LEFT] && charLocation.X > 0)
@@ -113,7 +113,7 @@ void update(double dt)
         Beep(1440, 30);
         charLocation.Y++; 
     }
-    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
+    if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 29)
     {
         Beep(1440, 30);
         charLocation.X++; 
@@ -141,13 +141,95 @@ void update(double dt)
         g_quitGame = true;    
 }
 
+//Jenny : print man
+void man()
+{
+	gotoXY(charLocation.X, charLocation.Y-11);
+	grey(1);
+	gotoXY(charLocation.X+10, charLocation.Y-11);
+	grey(1);
+	
+	gotoXY(charLocation.X, charLocation.Y-10);
+	grey(1);
+	gotoXY(charLocation.X+10, charLocation.Y-10);
+	grey(1);
+	
+	gotoXY(charLocation.X, charLocation.Y-9);
+	grey(11);
+
+	gotoXY(charLocation.X, charLocation.Y-8);
+	d_red(1);
+	grey(9);
+	
+	//Head
+	gotoXY(charLocation.X, charLocation.Y-7);
+	d_red(2);
+	gotoXY(charLocation.X+3, charLocation.Y-7);
+	d_red(5);
+	
+	gotoXY(charLocation.X+1, charLocation.Y-6);
+	d_red(1);
+	gotoXY(charLocation.X+3, charLocation.Y-6);
+	d_red(1);
+	black(1);
+	d_red(1);
+	black(1);
+	d_red(1);
+
+	gotoXY(charLocation.X+1, charLocation.Y-5);
+	d_red(1);
+	gotoXY(charLocation.X+3, charLocation.Y-5);
+	d_red(1);
+	black(1);
+	d_red(1);
+	black(1);
+	d_red(1);
+
+	gotoXY(charLocation.X+1, charLocation.Y-4);
+	d_red(1);
+	blue(1);
+	pink(1);
+	d_red(3);
+	pink(1);
+	blue(1);
+	d_red(1);
+
+	gotoXY(charLocation.X+1, charLocation.Y-3);
+	d_red(1);
+	blue(1);
+	d_red(2);
+	red(1);
+	d_red(2);
+	blue(1);
+	d_red(1);
+	
+	//Shirt
+	gotoXY(charLocation.X+3, charLocation.Y-2);
+	blue(5);
+
+	gotoXY(charLocation.X+9, charLocation.Y-2);
+	d_red(1);
+	
+	//Pants
+	gotoXY(charLocation.X+4, charLocation.Y-1);
+	black(3);
+
+	gotoXY(charLocation.X+9, charLocation.Y-1);
+	d_red(2);
+
+	//Feet
+	gotoXY(charLocation.X+3, charLocation.Y);
+	d_red(2);
+
+	gotoXY(charLocation.X+6, charLocation.Y);
+	d_red(2);
+}
+
 void render()
 {
     // clear previous screen
-    colour(0x0F);
+    colour(0x3C);
     cls();
-
-    //render the game
 
     // render time taken to calculate this frame
     gotoXY(70, 0);
@@ -158,11 +240,9 @@ void render()
     colour(0x59);
     std::cout << elapsedTime << "secs" << std::endl;
 
-
-    // render character
-    gotoXY(charLocation);
-    colour(0x0C);
-    std::cout << (char)1;
+    //Jenny : render character
+    gotoXY(charLocation.X, charLocation.Y-59);
+	man();
 
 	//Shannon : Render Score System
 	displayscore();
@@ -172,6 +252,9 @@ void render()
 
 	//Shannon : Render Life System
 	RenderLife();
+
+	//Jenny: Render border
+	RenderBorder();
 
 	//Shannon : Render Level System
 	RenderLevel();
